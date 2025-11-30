74ba2a8f4deed864d5b14fed96f8a550373a2bb7
yuhna111 <yuhna0811@gmail.com>
2025-11-30 23:44:41 +0700

    Update QuanTriServiceImpl.java

--- src/Java/impl/QuanTriServiceImpl.java
+++ src/Java/impl/QuanTriServiceImpl.java
@@ -16,14 +16,10 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class QuanTriServiceImpl implements QuanTriService {
-
-    // Khai báo Dependencies
     private final GiaoDichRepo giaoDichRepo;
     private final BangGiaRepo bangGiaRepo;
     
     private static final Logger LOGGER = Logger.getLogger(QuanTriServiceImpl.class.getName());
-
-    // Constructor để nhận dependencies (DI)
     public QuanTriServiceImpl(GiaoDichRepo giaoDichRepo, BangGiaRepo bangGiaRepo) {
         this.giaoDichRepo = giaoDichRepo;
         this.bangGiaRepo = bangGiaRepo;
@@ -32,21 +28,17 @@ public class QuanTriServiceImpl implements QuanTriService {
 
     @Override
     public double tinhTongDoanhThu() {
-        // TODO: Triển khai logic: Gọi giaoDichRepo để lấy tổng phí các giao dịch PAID
-        
-        // Trả về giá trị giả định để test GUI
         return 12500000.00; 
     }
 
     @Override
     public List<BangGia> layDanhSachBangGia() {
-        // TODO: Triển khai logic: Gọi bangGiaRepo.findAll()
         return bangGiaRepo.findAll();
     }
 
     @Override
     public boolean capNhatBangGia(BangGia bangGia) {
-        // TODO: Triển khai logic: Gọi bangGiaRepo.update(bangGia)
         return bangGiaRepo.update(bangGia);
     }
 }
+
